0235b2e6dbb06fcdc07087a6ceb7f687f2607f81
extravert <349173537@qq.com>
2023-05-04 11:29:53 +0800

    cache

--- gee-cache/doc/geecache-day1/cache.go
+++ gee-cache/doc/geecache-day1/cache.go
@@ -13,9 +13,21 @@ type cache struct {
 
 func (c *cache) add(key string, value ByteView) {  // 这里的ByteView 是不是引入了外部包里的
 	c.mu.Lock()
-	defer c.mu.Unlock()
+	defer c.mu.Unlock()  // 在函数返回之前先执行这个操作 在此之前 函数中其他代码都无法访问c.mu
 	if c.lru == nil {
 		c.lru = lru.New(c.cacheBytes, nil)
 	}
 	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
 }
